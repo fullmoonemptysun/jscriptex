98cfecca39bb3c8c1d66394780b1277f102f8fec
fullmoonemptysun <v685958@gmail.com>
2024-02-24 15:03:54 -0700

    solved 09 and 10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    const strProcessed = str.toLowerCase().match(/[a-z0-9]+/g).join("");
+    const strReversed = strProcessed.split("").reverse().join("");
+
+    return(strProcessed === strReversed);
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    
+    num = (Number)(num);
+    if(num < 0){
+        return 'OOPS';
+    }
 
+    if(num === 0){
+        return 0;
+    }
+
+
+
+    let prev1step = 1;
+    let prev2step = 0;
+    let currentValue = 0;
+    for(let i = 2 ; i <= num; i++){
+        currentValue = prev1step + prev2step;
+        prev2step = prev1step;
+        prev1step = currentValue;
+    };
+
+    return prev1step;
 };
 
 // Do not edit below this line
