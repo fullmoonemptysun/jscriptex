18d2fe5d65f7f3e269fa19af2f536572fd7fc465
fullmoonemptysun <v685958@gmail.com>
2024-02-24 22:38:11 -0700

    solved 12

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
+const getTheTitles = function(bookInfo) {
+    const bookTitles = bookInfo.map(function(book){
+        return book.title;
+    })
 
+    return bookTitles;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(list) {
 
+    return list.reduce((oldest, person) => {
+        let personAge = (person.yearOfDeath)?(person.yearOfDeath - person.yearOfBirth):((new Date().getFullYear()) - person.yearOfBirth);
+        let oldestAge = (oldest.yearOfDeath)?(oldest.yearOfDeath - oldest.yearOfBirth):((new Date().getFullYear()) - oldest.yearOfBirth);
+
+        return oldestAge < personAge? person: oldest;
+    })
+
+
+
+    // let oldest = list[0];
+//     for(person of list){
+
+//       let personAge = (person.yearOfDeath)?(person.yearOfDeath - person.yearOfBirth):((new Date().getFullYear()) - person.yearOfBirth);
+//       let oldestAge = (oldest.yearOfDeath)?(oldest.yearOfDeath - oldest.yearOfBirth):((new Date().getFullYear()) - oldest.yearOfBirth);
+      
+//       if(oldestAge <= personAge){
+//         oldest = person;
+//       }      
+//     }
+
+//     return oldest;
 };
 
 // Do not edit below this line
